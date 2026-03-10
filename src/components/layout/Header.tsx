8da9587db5f54bb88724d89fbc4f50850d996297
Pavel Hanyš <pavhanys@gmail.com>
2026-03-10 14:09:37 +0100

    Remove buy a sell pages

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,15 +1,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { MenuItem } from '@/components/layout/MenuItem';
 import { Button } from '@/components/ui/button';
-import {
-  NavigationMenu,
-  NavigationMenuList,
-} from '@/components/ui/navigation-menu';
 
 export function Header() {
   return (
-    <header className="bg-white sticky py-2 box-shadow top-0 z-10">
+    <header className="bg-white sticky py-2 shadow top-0 z-10">
       <div className="container">
         <div className="flex items-center justify-between gap-8">
           <div className="flex gap-8 items-center">
@@ -21,12 +16,6 @@ export function Header() {
                 height={30}
               />
             </Link>
-            <NavigationMenu>
-              <NavigationMenuList>
-                <MenuItem href="/buy">Koupit</MenuItem>
-                <MenuItem href="/sell">Prodat</MenuItem>
-              </NavigationMenuList>
-            </NavigationMenu>
           </div>
           <Button asChild className="primary">
             <Link href="/cars/new">Přidat auto</Link>
