1a074836e46fb61115ba8c2a16927d819b2c550c
Pavel Hanyš <pavhanys@gmail.com>
2026-03-10 16:23:56 +0100

    feat: initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,10 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Header } from "@/components/layout/Header";
 import { Toaster } from "@/components/ui/sonner";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const euclidCircularB = localFont({
   src: "../../public/fonts/euclid-circular-b/Euclid-Circular-B-Regular.ttf",
@@ -32,7 +36,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body className={`${euclidCircularB.variable} font-sans antialiased`}>
         <Header />
         <Toaster />
