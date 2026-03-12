891b607bac34c55792d18315a8c295470792a3ac
Pavel Hanyš <pavhanys@gmail.com>
2026-03-12 11:03:14 +0100

    Přepiš service worker

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { OfflineCacheToggle } from "@/components/layout/OfflineCacheToggle";
 import { Button } from "@/components/ui/button";
 
 export function Header() {
@@ -18,9 +19,12 @@ export function Header() {
               />
             </Link>
           </div>
-          <Button asChild className="primary" size="lg">
-            <Link href="/cars/new">Přidat auto</Link>
-          </Button>
+          <div className="flex items-center gap-4">
+            <OfflineCacheToggle />
+            <Button asChild className="primary" size="lg">
+              <Link href="/cars/new">Přidat auto</Link>
+            </Button>
+          </div>
         </div>
       </div>
     </header>
